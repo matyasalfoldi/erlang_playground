b6c31d608990205b0eccdec662ed73f893e2998e
Gam <matyas.alfoldi@outlook.com>
2023-12-24 12:42:44 +0100

    Generalize min for htol

--- htol_patternmatch.erl
+++ htol_patternmatch.erl
@@ -12,26 +12,19 @@ parse_map([]) -> [];
 parse_map([A,B,X|Rest]) ->
     [{{a,A},{b,B},{x,X}}] ++ parse_map(Rest).
 
-min(Path1, Path2, Path3, Path4) ->
-    Cost1 = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path1),
-    Cost2 = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path2),
-    Cost3 = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path3),
-    Cost4 = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path4),
-    MinCost = lists:min([Cost1,Cost2,Cost3,Cost4]),
-    case MinCost of
-        Cost1 -> Path1;
-        Cost2 -> Path2;
-        Cost3 -> Path3;
-        Cost4 -> Path4
-    end.
+min(Path) ->
+    min(Path, {notset, 0}).
 
-min(Path1, Path2) ->
-    Cost1 = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path1),
-    Cost2 = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path2),
-    MinCost = lists:min([Cost1,Cost2]),
-    case MinCost of
-        Cost1 -> Path1;
-        Cost2 -> Path2
+min([Path|RestPath], {notset, 0}) ->
+    Cost = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path),
+    min(RestPath, {Path, Cost});
+min([], {Path, _}) -> Path;
+min([Path|RestPath], {MinPath, MinCost}) ->
+    Cost = lists:foldl(fun({_, Val}, Acc) -> Acc + Val end, 0, Path),
+    if Cost =< MinCost ->
+        min(RestPath, {Path, Cost});
+    Cost > MinCost ->
+        min(RestPath, {MinPath, MinCost})
     end.
 
 calc_path([{{a,A},{b,B},{x,X}}|Rest]) ->
@@ -39,7 +32,7 @@ calc_path([{{a,A},{b,B},{x,X}}|Rest]) ->
     Path2 = calc_path(Rest, [{x,X},{a,A}]),
     Path3 = calc_path(Rest, [{b,B}]),
     Path4 = calc_path(Rest, [{x,X},{b,B}]),
-    min(Path1,Path2,Path3,Path4).
+    min([Path1,Path2,Path3,Path4]).
 
 calc_path([{{a,_},{b,B},{x,0}}], [{x,X1},{a,A1}|RestPath]) ->
     [{b,B},{x,X1},{a,A1}|RestPath];
@@ -52,16 +45,16 @@ calc_path([{{a,_},{b,B},{x,0}}], [{b,B1}|RestPath]) ->
 calc_path([{{a,_},{b,B},{x,X}}|Rest], [{x,X1},{a,A1}|RestPath]) ->
     Path1 = calc_path(Rest, [{b,B},{x,X1},{a,A1}|RestPath]),
     Path2 = calc_path(Rest, [{x,X},{b,B},{x,X1},{a,A1}|RestPath]),
-    min(Path1, Path2);
+    min([Path1, Path2]);
 calc_path([{{a,A},{b,_},{x,X}}|Rest], [{x,X1},{b,B1}|RestPath]) ->
     Path1 = calc_path(Rest, [{a,A},{x,X1},{b,B1}|RestPath]),
     Path2 = calc_path(Rest, [{x,X},{a,A},{x,X1},{b,B1}|RestPath]),
-    min(Path1, Path2);
+    min([Path1, Path2]);
 calc_path([{{a,A},{b,_},{x,X}}|Rest], [{a,A1}|RestPath]) ->
     Path1 = calc_path(Rest, [{a,A},{a,A1}|RestPath]),
     Path2 = calc_path(Rest, [{x,X},{a,A},{a,A1}|RestPath]),
-    min(Path1, Path2);
+    min([Path1, Path2]);
 calc_path([{{a,_},{b,B},{x,X}}|Rest], [{b,B1}|RestPath]) ->
     Path1 = calc_path(Rest, [{b,B},{b,B1}|RestPath]),
     Path2 = calc_path(Rest, [{x,X},{b,B},{b,B1}|RestPath]),
-    min(Path1, Path2).
+    min([Path1, Path2]).
